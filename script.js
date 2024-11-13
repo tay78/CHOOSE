69011c9efc4145339ada19f7040e978c7f2810ee
William Starzyk <wstarzyk@cps.edu>
2024-11-13 18:30:33 +0000

    subwayOfHell

--- script.js
+++ script.js
@@ -4,24 +4,44 @@ let headButton = document.querySelector('.head-button');
 
 
 // html content elements
-let choice1image = document.querySelector('.choice1image');
 let text = document.querySelector('.text');
+let pizza = document.querySelector('h2');
 
-let choice1Aimage = document.querySelector('.choice1Aimage');
-choice1Aimage.style.display ="none";
-
-
-
+// html images elements
+let start = document.querySelector('.startSubway');
+    
+let choiceAimage = document.querySelector('.choiceAimage');
+    choiceAimage.style.display ="none";
+let choiceA1image = document.querySelector('.choiceA1image');
+    choiceA1image.style.display ="none";
+let choiceA2image = document.querySelector('.choiceA2image');
+    choiceA2image.style.display ="none";
 
 headButton.addEventListener('click', function() {
-text.innerHTML = "The tracks lead you deeper into the subway tunnels. You encounter a scavenger who is hostile. Double click picture to fight off. ";
-choice1image.style.display = "none";
-choice1Aimage.style.display ="block";
+text.innerHTML = "The tracks lead you deeper into the subway tunnels. You encounter a scavenger who is hostile. Double click picture to fight OR seach the page for supplies ";
+start.style.display = "none";
+choiceAimage.style.display ="block";
 exploreButton.style.display="none";
 headButton.style.display="none";
 });
 
-choice1Aimage.addEventListener('dblclick', function(){
-    text.innerHTML = "You make it another day"
-    choice1Aimage.src = "knockedout.jpg";
+choiceAimage.addEventListener('dblclick', function(){
+    choiceAimage.style.display ="none";
+    text.innerHTML = "You knock them out. You are safe for now"
+    choiceA1image.style.display ="block";
+});
+
+pizza.addEventListener("mouseenter", function(){
+    choiceAimage.style.display ="none";
+    text.innerHTML = "You found supplies. You make it another day"
+    choiceA2image.style.display ="block";
+});
+
+
+
+
+exploreButton.addEventListener("click", function(){
+    
+    text.innerHTML = "SORRY THIS CHOICE IS STILL UNDER CONSTRUCTION"
+   
 });
