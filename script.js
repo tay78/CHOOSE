7314358ff474575591dd14d3f68bcce12ab7870a
tay78 <tkwilliams-s@cps.edu>
2024-11-14 17:28:07 +0000

    incomplete have to add up button

--- script.js
+++ script.js
@@ -16,9 +16,11 @@ let choiceA1image = document.querySelector('.choiceA1image');
     choiceA1image.style.display ="none";
 let choiceA2image = document.querySelector('.choiceA2image');
     choiceA2image.style.display ="none";
+let choiceA3image = document.querySelector('.choiceA3image');
+    choiceA3image.style.display ="none";
 
 headButton.addEventListener('click', function() {
-text.innerHTML = "The tracks lead you deeper into the subway tunnels. You encounter a scavenger who is hostile. Double click picture to fight OR seach the page for supplies ";
+text.innerHTML = "You go inside and start feeling hungry. You hear a loud noise. Double click picture to Investigate OR stay where you are and search the page for food ";
 start.style.display = "none";
 choiceAimage.style.display ="block";
 exploreButton.style.display="none";
@@ -27,7 +29,7 @@ headButton.style.display="none";
 
 choiceAimage.addEventListener('dblclick', function(){
     choiceAimage.style.display ="none";
-    text.innerHTML = "You knock them out. You are safe for now"
+    text.innerHTML = "You see a shining light deep in the cave and the chanting sounds grow louder as you approach you feel a sharp pain in your head everything goes dark"
     choiceA1image.style.display ="block";
 });
 
@@ -41,7 +43,9 @@ pizza.addEventListener("mouseenter", function(){
 
 
 exploreButton.addEventListener("click", function(){
-    
-    text.innerHTML = "SORRY THIS CHOICE IS STILL UNDER CONSTRUCTION"
-   
+    start.style.display = "none";
+    exploreButton.style.display="none";
+headButton.style.display="none";
+    text.innerHTML = "You can't see go find things on the page to make fire or click the up key to continue wandering"
+    choiceA3image.style.display ="block";
 });
